bbdd59f8013ebbd4ac77643b2b15a5d4cc6f5a23
Richy Gomez <richy_gomez29@outlook.com>
2023-03-28 09:55:41 +0200

    dawg

--- script.js
+++ script.js
@@ -5,47 +5,47 @@ const likeCounter = document.querySelector('#like-counter');
 const dislikeCounter = document.querySelector('#dislike-counter');
 
 fetch('https://dog.ceo/api/breeds/image/random')
-  .then(response => response.json())
-  .then(data => {
-    dogImage.src = data.message;
-  });
-
-likeButton.addEventListener('click', () => {
-  let likes = parseInt(likeCounter.textContent);
-  likes++;
-  likeCounter.textContent = likes;
-
-  localStorage.setItem('likes', likes);
-
-  fetch('https://dog.ceo/api/breeds/image/random')
     .then(response => response.json())
     .then(data => {
-      dogImage.src = data.message;
+        dogImage.src = data.message;
     });
+
+likeButton.addEventListener('click', () => {
+    let likes = parseInt(likeCounter.textContent);
+    likes++;
+    likeCounter.textContent = likes;
+
+    localStorage.setItem('likes', likes);
+
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => response.json())
+        .then(data => {
+            dogImage.src = data.message;
+        });
 });
 
 dislikeButton.addEventListener('click', () => {
-  let dislikes = parseInt(dislikeCounter.textContent);
-  dislikes++;
-  dislikeCounter.textContent = dislikes;
+    let dislikes = parseInt(dislikeCounter.textContent);
+    dislikes++;
+    dislikeCounter.textContent = dislikes;
 
-  localStorage.setItem('dislikes', dislikes);
+    localStorage.setItem('dislikes', dislikes);
 
-  fetch('https://dog.ceo/api/breeds/image/random')
-    .then(response => response.json())
-    .then(data => {
-      dogImage.src = data.message;
-    });
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => response.json())
+        .then(data => {
+            dogImage.src = data.message;
+        });
 });
 
 window.addEventListener('load', () => {
-  const likes = localStorage.getItem('likes');
-  if (likes) {
-    likeCounter.textContent = likes;
-  }
+    const likes = localStorage.getItem('likes');
+    if (likes) {
+        likeCounter.textContent = likes;
+    }
 
-  const dislikes = localStorage.getItem('dislikes');
-  if (dislikes) {
-    dislikeCounter.textContent = dislikes;
-  }
+    const dislikes = localStorage.getItem('dislikes');
+    if (dislikes) {
+        dislikeCounter.textContent = dislikes;
+    }
 });
