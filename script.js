c57d89866fbef85279ff08d466bc18b448f802f1
Richy Gomez <richy_gomez29@outlook.com>
2023-03-28 09:22:28 +0200

    the dawg in me

---
+++ script.js
@@ -0,0 +1,51 @@
+const dogImage = document.querySelector('#dog-image');
+const likeButton = document.querySelector('#like-button');
+const dislikeButton = document.querySelector('#dislike-button');
+const likeCounter = document.querySelector('#like-counter');
+const dislikeCounter = document.querySelector('#dislike-counter');
+
+fetch('https://dog.ceo/api/breeds/image/random')
+.then(response => response.json())
+.then(data => {
+    dogImage.src = data.message;
+});
+
+likeButton.addEventListener('click', () => {
+    let likes = parseInt(likeCounter.textContent);
+    likes++;
+    likeCounter.textContent = likes;
+    
+    localStorage.setItem('likes', likes);
+    
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(data => {
+        dogImage.src = data.message;
+    });
+});
+
+dislikeButton.addEventListener('click', () => {
+    let dislikes = parseInt(dislikeCounter.textContent);
+    dislikes++;
+    dislikeCounter.textContent = dislikes;
+    
+    localStorage.setItem('dislikes', dislikes);
+    
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(data => {
+        dogImage.src = data.message;
+    });
+});
+
+window.addEventListener('load', () => {
+    const likes = localStorage.getItem('likes');
+    if (likes) {
+        likeCounter.textContent = likes;
+    }
+    
+    const dislikes = localStorage.getItem('dislikes');
+    if (dislikes) {
+        dislikeCounter.textContent = dislikes;
+    }
+});
